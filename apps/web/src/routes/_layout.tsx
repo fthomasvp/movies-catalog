af992b3880e2c6f6aff5beae0a15fecad51b764d
fthomasvp <fellipecab@gmail.com>
2025-01-28 00:06:32 +0000

    WIP: add TanStack Router

---
+++ apps/web/src/routes/_layout.tsx
@@ -0,0 +1,13 @@
+import { Outlet, createFileRoute } from "@tanstack/react-router";
+
+export const Route = createFileRoute("/_layout")({
+  component: RouteComponent,
+});
+
+function RouteComponent() {
+  return (
+    <div>
+      Hello "/_layout"! <Outlet />{" "}
+    </div>
+  );
+}
