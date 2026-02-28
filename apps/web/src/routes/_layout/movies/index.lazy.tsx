237dfbe3830c270f8a05c5dc44dae5b5faffb6c6
fthomasvp <fellipecab@gmail.com>
2026-02-28 17:30:46 +0000

    refactor: ignore TanStack Router generated in Biome
    - Format files from Web package

--- apps/web/src/routes/_layout/movies/index.lazy.tsx
+++ apps/web/src/routes/_layout/movies/index.lazy.tsx
@@ -1,9 +1,9 @@
-import { createLazyFileRoute } from '@tanstack/react-router'
+import { createLazyFileRoute } from "@tanstack/react-router";
 
-export const Route = createLazyFileRoute('/_layout/movies/')({
+export const Route = createLazyFileRoute("/_layout/movies/")({
   component: RouteComponent,
-})
+});
 
 function RouteComponent() {
-  return <div>Hello "/_layout/movies/"!</div>
+  return <div>Hello "/_layout/movies/"!</div>;
 }
